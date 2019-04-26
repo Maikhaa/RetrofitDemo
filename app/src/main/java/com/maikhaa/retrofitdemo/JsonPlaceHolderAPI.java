9bd9c3114857ed9647e3815a5965796aaa1afdb3
Maikhaa <maikha.abarca@gmail.com>
2019-04-26 21:24:37 +0800

    first commit

---
+++ app/src/main/java/com/maikhaa/retrofitdemo/JsonPlaceHolderAPI.java
@@ -0,0 +1,13 @@
+package com.maikhaa.retrofitdemo;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface JsonPlaceHolderAPI {
+
+    @GET("posts")
+    Call<List<Post>> getPosts();
+
+}
